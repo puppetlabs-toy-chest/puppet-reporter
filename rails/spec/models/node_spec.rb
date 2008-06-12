577b7f3e6dce253904c8f8fc997df44105247ebc
Rick Bradley <rick@rickbradley.com>
2008-06-12 18:09:32 -0500

    trivial node spec

--- rails/spec/models/node_spec.rb
+++ rails/spec/models/node_spec.rb
@@ -8,4 +8,14 @@ describe Node do
   it "should be valid" do
     @node.should be_valid
   end
+  
+  describe 'attributes' do
+    before :each do
+      @node = Node.new
+    end
+    
+    it 'should have a name' do
+      @node.name.should be_nil
+    end
+  end
 end
