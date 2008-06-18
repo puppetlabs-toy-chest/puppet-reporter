3cf61e44245cbe72f7185ad64c3446ad545c0654
Rick Bradley <rick@rickbradley.com>
2008-06-18 16:13:19 -0500

    adding robustness for when reports are requested that don't exist

--- rails/app/controllers/reports_controller.rb
+++ rails/app/controllers/reports_controller.rb
@@ -1,6 +1,8 @@
 class ReportsController < ApplicationController
   def show
     @report = Report.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:notice] = 'Could not find information for that report.'
+    return redirect_to(reports_path)
   end
-
 end
--- rails/spec/controllers/reports_controller_spec.rb
+++ rails/spec/controllers/reports_controller_spec.rb
@@ -4,4 +4,25 @@ describe ReportsController do
 
   it_should_behave_like 'a RESTful controller with a show action'
   
+  describe 'show' do
+    def do_get(params)
+      get :show, {}.merge(params)
+    end
+    
+    describe 'when no matching report can be found' do
+      before :each do
+        Report.expects(:find).raises(ActiveRecord::RecordNotFound)
+      end
+      
+      it 'should set a flash warning about the failure' do
+        do_get('id' => '1')
+        flash[:notice].should match(/find/)
+      end
+      
+      it 'should redirect to the report index page' do
+        do_get('id' => '1')
+        response.should redirect_to(reports_path)        
+      end
+    end
+  end
 end
