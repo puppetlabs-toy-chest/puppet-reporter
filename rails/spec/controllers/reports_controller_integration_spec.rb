bbf89a2ee74cf4bd6c5f1c7a81f3605e4081cf6f
Rick Bradley <rick@rickbradley.com>
2008-06-18 16:15:18 -0500

    basic integration test for reports controller #show action

---
+++ rails/spec/controllers/reports_controller_integration_spec.rb
@@ -0,0 +1,44 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe ReportsController, "when running integrations" do
+  integrate_views
+
+  describe 'show' do
+    before :all do
+      Report.delete_all
+    end
+    
+    def do_request(params)
+      get :show, {}.merge(params)
+    end
+    
+    describe 'when no matching report exists' do
+      it 'should set a flash message about the report not matching' do
+        do_request('id' => '1')
+        flash[:notice].should match(/find/)
+      end
+      
+      it 'should redirect to the report index page' do
+        do_request('id' => '1')
+        response.should redirect_to(reports_path)
+      end
+    end
+    
+    describe 'when a matching report exists' do
+      before :all do
+        @node = Node.generate
+        @report = @node.reports.generate
+      end
+      
+      it 'should make the report available to the view' do
+        do_request('id' => @report.id)
+        assigns[:report].should == @report
+      end
+      
+      it 'should show the report page' do
+        do_request('id' => @report.id)
+        response.should render_template('show')
+      end      
+    end
+  end
+end
