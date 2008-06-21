1ca351b87bee4ce0c6ee35517402b49f54225f51
Yossef Mendelssohn <ymendel@pobox.com>
2008-06-20 19:12:01 -0500

    Refactoring repeated code in Metric.total_changes_between and .total_failures_between

--- rails/app/models/metric.rb
+++ rails/app/models/metric.rb
@@ -19,56 +19,41 @@ class Metric < ActiveRecord::Base
       metrics.inject({}) {|h, m| h[m.category] ||= []; h[m.category] << m; h}
     end
     
-    def total_changes_between(start_time, end_time, options = {})
-      find_options = {}
-      find_options[:include] = :report unless ((scope(:find) || {})[:joins] || '').match(/reports\.id/)
-      
-      if interval = options[:interval]
-        metrics = []
-        low_time = start_time
-        high_time = low_time + interval
-        
-        while high_time <= end_time
-          metrics.push find(:all, { :conditions => ['reports.timestamp >= ? and reports.timestamp < ? and category = ? and label = ?', low_time, high_time, 'changes', 'Total'] }.merge(find_options))
+    def self.define_time_interval_method(type, data)
+      class_eval <<-end_of_eval
+        def total_#{type}_between(start_time, end_time, options = {})
+          find_options = {}
+          find_options[:include] = :report unless ((scope(:find) || {})[:joins] || '').match(/reports\.id/)
           
-          low_time   = high_time
-          high_time += interval
-          if high_time > end_time
-            high_time = end_time unless low_time == end_time
+          if interval = options[:interval]
+            metrics = []
+            low_time = start_time
+            high_time = low_time + interval
+            
+            while high_time <= end_time
+              metrics.push find(:all, { :conditions => ['reports.timestamp >= ? and reports.timestamp < ? and category = ? and label = ?', low_time, high_time, '#{data[:category]}', '#{data[:label]}'] }.merge(find_options))
+              
+              low_time   = high_time
+              high_time += interval
+              if high_time > end_time
+                high_time = end_time unless low_time == end_time
+              end
+            end
+            
+            metrics.collect { |met|  met.collect(&:value).inject(&:+) || 0 }
+          else
+            metrics = find(:all, { :conditions => ['reports.timestamp >= ? and reports.timestamp < ? and category = ? and label = ?', start_time, end_time, '#{data[:category]}', '#{data[:label]}'] }.merge(find_options))
+            metrics.collect(&:value).inject(&:+) || 0
           end
         end
-        
-        metrics.collect { |met|  met.collect(&:value).inject(&:+) || 0 }
-      else
-        metrics = find(:all, { :conditions => ['reports.timestamp >= ? and reports.timestamp < ? and category = ? and label = ?', start_time, end_time, 'changes', 'Total'] }.merge(find_options))
-        metrics.collect(&:value).inject(&:+) || 0
-      end
+      end_of_eval
     end
     
-    def total_failures_between(start_time, end_time, options = {})
-      find_options = {}
-      find_options[:include] = :report unless ((scope(:find) || {})[:joins] || '').match(/reports\.id/)
-      
-      if interval = options[:interval]
-        metrics = []
-        low_time = start_time
-        high_time = low_time + interval
-        
-        while high_time <= end_time
-          metrics.push find(:all, { :conditions => ['reports.timestamp >= ? and reports.timestamp < ? and category = ? and label = ?', low_time, high_time, 'resources', 'Failed'] }.merge(find_options))
-          
-          low_time   = high_time
-          high_time += interval
-          if high_time > end_time
-            high_time = end_time unless low_time == end_time
-          end
-        end
-        
-        metrics.collect { |met|  met.collect(&:value).inject(&:+) || 0 }
-      else
-        metrics = find(:all, { :conditions => ['reports.timestamp >= ? and reports.timestamp < ? and category = ? and label = ?', start_time, end_time, 'resources', 'Failed'] }.merge(find_options))
-        metrics.collect(&:value).inject(&:+) || 0
-      end
+    {
+      :changes  => { :category => 'changes',   :label => 'Total' },
+      :failures => { :category => 'resources', :label => 'Failed' }
+    }.each do |type, data|
+      define_time_interval_method(type, data)
     end
   end
 end
