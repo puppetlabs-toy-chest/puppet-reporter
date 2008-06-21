90ce4612b24678e5389400345579c67d154cec62
Yossef Mendelssohn <ymendel@pobox.com>
2008-06-21 15:43:46 -0500

    Adding failure list to node show view.

--- rails/spec/views/nodes/show.html.erb_spec.rb
+++ rails/spec/views/nodes/show.html.erb_spec.rb
@@ -2,7 +2,7 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe '/nodes/show.html.erb' do
   before :each do
-    @node = Node.new(:name => 'foo')
+    @node = Node.generate!(:name => 'foo')
     assigns[:node] = @node
   end
 
@@ -52,5 +52,74 @@ describe '/nodes/show.html.erb' do
       do_render
       response.should have_tag('a[href=?]', report_path(@report))
     end  
+  end
+
+  describe 'if a node has failures' do
+    before :each do
+      @report  = @node.reports.generate!
+      @failure = @report.metrics.generate!(:category => 'resources', :label => 'Failed', :value => 3)
+      @node.reload
+    end
+    
+    it 'should include the node failures' do
+      do_render
+      response.should have_tag('ul[id=?]', 'failure_list')
+    end
+    
+    it 'should include a list item for the failure' do
+      do_render
+      response.should have_tag('ul[id=?]', 'failure_list') do
+        with_tag('li')
+      end
+    end
+    
+    it 'should include the failure count' do
+      do_render
+      response.should have_tag('ul[id=?]', 'failure_list') do
+        with_tag('li', :text => Regexp.new(Regexp.escape(@failure.value.to_s)))
+      end
+    end
+    
+    it 'should include a link to the report' do
+      do_render
+      response.should have_tag('ul[id=?]', 'failure_list') do
+        with_tag('li') do
+          with_tag('a[href=?]', report_path(@report))
+        end
+      end
+    end
+    
+    it 'should include the report time' do
+      do_render
+      response.should have_tag('ul[id=?]', 'failure_list') do
+        with_tag('li', :text => Regexp.new(Regexp.escape(@report.timestamp.to_s)))
+      end
+    end
+    
+    it 'should include an item for every failure' do
+      other_report  = @node.reports.generate!(:timestamp => Time.zone.now + 1235)
+      other_failure = other_report.metrics.generate!(:category => 'resources', :label => 'Failed', :value => 3)
+      @node.reload
+      
+      do_render
+      response.should have_tag('ul[id=?]', 'failure_list') do
+        [@report, other_report].each do |report|
+          with_tag('li') do
+            with_tag('a[href=?]', report_path(report))
+          end
+        end
+      end
+    end
+  end
+
+  describe 'if a node has no failures' do
+    before :each do
+      @node.stubs(:failures).returns([])
+    end
+    
+    it 'should not include any failures' do
+      do_render
+      response.should_not have_tag('ul[id=?]', 'failure_list')
+    end
   end
 end
