5e35ed471bd15588ba41f75deb7eae1d8464fbac
Rick Bradley <rick@rickbradley.com>
2008-06-22 17:11:00 -0500

    trivial cleanup

--- rails/puppet/report/puppet_show.rb
+++ rails/puppet/report/puppet_show.rb
@@ -1,22 +1,21 @@
 require 'puppet/reports'
 
+def connection_settings
+  { :host => 'localhost', :port => 80 }
+end
+
 def submit_yaml_report_to_puppetshow(report)
   network_post report.to_yaml
 end
 
-# TODO:  untested -- derived from main Puppet REST code
 def network_post(data)
   network {|conn| conn.post("/nodes/create", data).body }
 end
 
-# TODO:  untested -- derived from main Puppet REST code
 def network(&block)
   Net::HTTP.start(connection_settings[:host], connection_settings[:port]) {|conn| yield(conn) }
 end
 
-def connection_settings
-  { :host => 'localhost', :port => 80 }
-end
 
 Puppet::Reports.register_report(:puppet_show) do
   Puppet.settings.use(:reporting)
@@ -26,4 +25,3 @@ Puppet::Reports.register_report(:puppet_show) do
     submit_yaml_report_to_puppetshow(self)
   end
 end
-
