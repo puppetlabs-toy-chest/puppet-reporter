274b30e36826d438c59795ac283b16053c0e39df
Rick Bradley <rick@rickbradley.com>
2008-06-23 22:58:02 -0500

    adding failures specs for nodes controller

--- rails/spec/controllers/nodes_controller_integration_spec.rb
+++ rails/spec/controllers/nodes_controller_integration_spec.rb
@@ -59,4 +59,46 @@ describe NodesController, "when running integrations" do
       end
     end
   end
+  
+  describe 'failures' do
+    before :all do
+      Node.delete_all
+    end
+    
+    def do_request
+      get :failures, :id => 'foo'
+    end
+    
+    describe 'when no matching node exists' do
+      it 'should set a flash message about the node not matching' do
+        do_request
+        flash[:notice].should match(/find/)
+      end
+      
+      it 'should redirect to the node summary page' do
+        do_request
+        response.should redirect_to(nodes_path)
+      end
+    end
+    
+    describe 'when a matching node exists' do
+      before :all do
+        @node = Node.generate(:name => 'foo')
+      end
+      
+      it 'should make the node available to the view' do
+        pending("rspec learning about alternative templates") do
+          do_request
+          assigns[:node].should == @node
+        end
+      end
+      
+      it 'should show the failures page' do
+        pending("rspec learning about alternative templates") do |variable|
+          do_request
+          response.should render_template('failures.csv.erb')          
+        end
+      end
+    end
+  end
 end
--- rails/spec/controllers/nodes_controller_spec.rb
+++ rails/spec/controllers/nodes_controller_spec.rb
@@ -123,4 +123,123 @@ describe NodesController do
       end
     end
   end
+  
+  describe 'failures' do
+    def do_get(params = {})
+      get :failures, {}.merge(params)
+    end
+
+    it "should be successful" do
+      do_get('id' => '1')
+      response.should be_success
+    end
+  
+    it "should render the failures template" do
+      do_get('id' => '1')
+      response.should render_template('failures')
+    end
+  
+    it "should find the node requested" do
+      Node.expects(:find).with('1').returns(@node)
+      do_get('id' => '1')
+    end
+
+    it "should make the found object available to the view" do
+      do_get('id' => '1')
+      assigns[:node].should == @node
+    end
+
+    describe 'when a node name is specified' do
+      it 'should look up the node by name' do
+        Node.expects(:find_by_name).returns(@node)
+        do_get('id' => 'foo')
+      end
+      
+      describe 'and the node cannot be found' do
+        before :each do
+          Node.stubs(:find_by_name).returns(nil)
+        end
+        
+        it 'should set a flash message about not being able to find the node' do
+          do_get('id' => 'foo')
+          flash[:notice].should match(/find/)
+        end
+        
+        it 'should redirect to the node index page' do
+          do_get('id' => 'foo')
+          response.should redirect_to(nodes_path)
+        end
+      end
+      
+      describe 'and the node can be found' do
+        before :each do
+          Node.stubs(:find_by_name).returns(@node)   
+          @failures = stub('failures')       
+        end
+
+        it 'should make the node available to the view' do
+          do_get('id' => 'foo')
+          assigns[:node].should == @node
+        end
+        
+        it 'should render the node failures page' do
+          do_get('id' => 'foo')
+          response.should render_template('failures')
+        end
+        
+        it 'should render failures as text' do
+          do_get('id' => 'foo')
+          response.content_type.should == 'text/plain'
+        end
+        
+        it 'should look up the failures for the node' do
+          @node.expects(:failures).returns(@failures)
+          do_get('id' => 'foo')
+        end
+                
+        it 'should make the most recent node report available to the view' do
+          @node.stubs(:failures).returns(@failures)
+          do_get('id' => 'foo')
+          assigns[:failures].should == @failures
+        end
+      end
+    end
+    
+    describe 'when a node name is not specified' do
+      describe 'but a numeric id is specified' do
+        it 'should not attempt to look up the node by name' do
+          Node.expects(:find_by_name).never
+          do_get('id' => 1)
+        end
+        
+        describe 'and the node with that id cannot be found' do
+          before :each do
+            Node.stubs(:find).raises(ActiveRecord::RecordNotFound)
+          end
+          
+          it 'should set a flash message about not being able to find the node' do
+            do_get('id' => 1)
+            flash[:notice].should match(/find/)
+          end
+          
+          it 'should redirect to the node index page' do
+            do_get('id' => 1)
+            response.should redirect_to(nodes_path)
+          end
+        end
+      end
+    end
+
+    describe 'when neither a node name or an id is specified' do
+      it 'should set a flash message about not being able to find the node' do
+        do_get
+        flash[:notice].should match(/find/)
+      end
+      
+      it 'should redirect to the node index page' do
+        do_get
+        response.should redirect_to(nodes_path)
+      end
+    end
+  end
 end
