f92ac4787b044ee274171d3c114ba6b37f9b44af
Rick Bradley <rick@rickbradley.com>
2008-06-23 23:21:55 -0500

    sticking hpricot gem in vendor for XML builder tests for timeplot/timeline event plotting for reports

--- rails/config/environment.rb
+++ rails/config/environment.rb
@@ -27,6 +27,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
 
   config.gem "sparklines"
+  config.gem "hpricot"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
