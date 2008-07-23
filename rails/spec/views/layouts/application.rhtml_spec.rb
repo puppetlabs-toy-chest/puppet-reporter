a04b9c1388a22f595eb8a87b6ec3bf6e24a3b50b
Yossef Mendelssohn <ymendel@pobox.com>
2008-07-23 10:39:35 -0500

    Adding Juggernaut JS stuff to layout.

---
+++ rails/spec/views/layouts/application.rhtml_spec.rb
@@ -0,0 +1,17 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe '/layouts/application.rhtml' do
+  def do_render
+    render '/layouts/application.rhtml'
+  end
+  
+  it 'should include juggernaut JS' do
+    do_render
+    response.should have_text(/juggernaut\.js/)
+  end
+  
+  it 'should create a juggernaut object' do
+    do_render
+    response.should have_text(/new Juggernaut/)
+  end
+end
