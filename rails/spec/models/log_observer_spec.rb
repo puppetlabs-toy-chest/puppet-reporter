6ae49f5bf8a74a80b5c257f8946d2df82de42638
Yossef Mendelssohn <ymendel@pobox.com>
2008-07-23 17:20:04 -0500

    Turning off observers for test.
    At this point, just ensuring that the LogObserver using Juggernaut won't make testing a pain.
    This LogObserver spec also had to be changed because LogObserver.instance registers the observer anyway and I couldn't find an easy/obvious way to unregister it.

--- rails/spec/models/log_observer_spec.rb
+++ rails/spec/models/log_observer_spec.rb
@@ -1,6 +1,12 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe LogObserver do
+  
+  class LogObserver
+    def initialize
+    end
+  end
+  
   before :each do
     @observer = LogObserver.instance
   end
