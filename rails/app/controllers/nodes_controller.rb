66e90944e99c0fa88270566df343bb93b3e2d28d
Yossef Mendelssohn <ymendel@pobox.com>
2008-07-30 16:22:44 -0500

    Refactoring repeated node-finding code in NodesController.

--- rails/app/controllers/nodes_controller.rb
+++ rails/app/controllers/nodes_controller.rb
@@ -1,52 +1,40 @@
 class NodesController < ApplicationController
+  before_filter :get_node, :only => [:show, :failures, :reports]
+  
   def index
     @nodes = Node.find(:all)
   end
   
   def show
-    if params[:id] =~ /^\d+$/
-      @node = Node.find(params[:id]) rescue nil
-    else
-      @node = Node.find_by_name(CGI.unescape(params[:id])) unless params[:id].blank?
-    end
-    
-    unless @node
-      flash[:notice] = 'Could not find information for that node.'
-      return redirect_to(nodes_path)
-    end
-    
     @most_recent_report = @node.most_recent_report_on(Time.zone.now)
   end
   
   def failures
-    if params[:id] =~ /^\d+$/
-      @node = Node.find(params[:id]) rescue nil
-    else
-      @node = Node.find_by_name(CGI.unescape(params[:id])) unless params[:id].blank?
-    end
-    
-    unless @node
-      flash[:notice] = 'Could not find information for that node.'
-      return redirect_to(nodes_path)
-    end
-    
     @failures = @node.failures
     render :layout => false, :content_type => 'text/plain'
   end
 
   
   def reports
+    @reports = @node.reports.find(:all, :limit => 100, :order => 'timestamp desc').sort_by(&:timestamp)
+  end
+  
+  
+  private
+  
+  def get_node
     if params[:id] =~ /^\d+$/
       @node = Node.find(params[:id]) rescue nil
     else
       @node = Node.find_by_name(CGI.unescape(params[:id])) unless params[:id].blank?
     end
     
-    unless @node
+    if @node
+      true
+    else
       flash[:notice] = 'Could not find information for that node.'
-      return redirect_to(nodes_path)
+      redirect_to(nodes_path)
+      false
     end
-    
-    @reports = @node.reports.find(:all, :limit => 100, :order => 'timestamp desc').sort_by(&:timestamp)
   end
 end
