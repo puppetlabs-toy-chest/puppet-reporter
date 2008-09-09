c3e778d2755d7737fa87942319339a30ecb90072
Yossef Mendelssohn <ymendel@pobox.com>
2008-09-09 14:54:30 -0500

    Adding flot to application layout

--- rails/spec/views/layouts/application.rhtml_spec.rb
+++ rails/spec/views/layouts/application.rhtml_spec.rb
@@ -9,4 +9,9 @@ describe '/layouts/application.rhtml' do
     do_render
     response.should have_text(/juggernaut\.js/)
   end
+  
+  it 'should include flot JS' do
+    do_render
+    response.should have_text(/jquery\.flot\.js/)
+  end
 end
