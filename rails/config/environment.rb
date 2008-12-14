402bae2161008b0f7813cd7792e372d9007c2cd9
Rick Bradley <rick@rickbradley.com>
2008-12-13 22:15:42 -0600

    adding will_paginate gem

--- rails/config/environment.rb
+++ rails/config/environment.rb
@@ -28,7 +28,8 @@ Rails::Initializer.run do |config|
 
   config.gem "hpricot"
   config.gem 'juggernaut'
-
+  config.gem 'mislav-will_paginate', :version => '~> 2.2.3', :lib => 'will_paginate', :source => 'http://gems.github.com'
+  
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named
