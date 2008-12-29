925b33d624989141192c0310d0efbfc4ec010e1c
Rick Bradley <rick@rickbradley.com>
2008-12-29 14:13:49 -0600

    ensure that we're passing the requested page when searching

--- rails/app/controllers/dashboard_controller.rb
+++ rails/app/controllers/dashboard_controller.rb
@@ -2,12 +2,14 @@ class DashboardController < ApplicationController
   def index
     @failed_nodes = Node.failed
     @silent_nodes = Node.silent
-    
+
     @logs = Log.latest
   end
 
   def search
     @q = params[:q]
-    @results = Node.search(:conditions => SearchParser.parse(@q))
+    @results = Node.search(:conditions => SearchParser.parse(@q),
+                           :page => params[:page],
+                           :per_page => 2)
   end
 end
--- rails/spec/controllers/dashboard_controller_spec.rb
+++ rails/spec/controllers/dashboard_controller_spec.rb
@@ -5,48 +5,48 @@ describe DashboardController do
     before :each do
       @failed = stub('failed nodes')
       Node.stubs(:failed).returns(@failed)
-      
+
       @silent = stub('silent nodes')
       Node.stubs(:silent).returns(@silent)
-      
+
       @logs = stub('latest logs')
       Log.stubs(:latest).returns(@logs)
     end
-    
+
     def do_get
       get :index
     end
-    
+
     it 'should be successful' do
       do_get
       response.should be_success
     end
-    
+
     it 'should get failed nodes' do
       Node.expects(:failed)
       do_get
     end
-    
+
     it 'should assign failed nodes for the view' do
       do_get
       assigns[:failed_nodes].should == @failed
     end
-    
+
     it 'should get nodes not checked in' do
       Node.expects(:silent)
       do_get
     end
-    
+
     it 'should assign nodes not checked in for the view' do
       do_get
       assigns[:silent_nodes].should == @silent
     end
-    
+
     it 'should get latest logs' do
       Log.expects(:latest)
       do_get
     end
-    
+
     it 'should assign latest logs for the view' do
       do_get
       assigns[:logs].should == @logs
@@ -58,12 +58,13 @@ describe DashboardController do
       @query = 'match'
       @matches = 'matches'
       @parsed = 'parsed'
+      @page = '2'
       Node.stubs(:search).returns(@matches)
       SearchParser.stubs(:parse).returns(@parsed)
     end
-    
+
     def do_get
-      get :search, { :q => @query }
+      get :search, { :q => @query, :page => @page }
     end
 
     it 'should be successful' do
@@ -77,7 +78,16 @@ describe DashboardController do
     end
 
     it 'should search nodes with conditions from the parsed query string' do
-      Node.expects(:search).with({ :conditions => @parsed })
+      Node.expects(:search).with do |args|
+        args[:conditions] == @parsed
+      end
+      do_get
+    end
+
+    it 'should pass the current page when searching' do
+      Node.expects(:search).with do |args|
+        args[:page] == @page
+      end
       do_get
     end
 
@@ -85,12 +95,12 @@ describe DashboardController do
       do_get
       assigns[:q].should == @query
     end
-    
+
     it 'should make search results available to the view' do
       do_get
       assigns[:results].should == @matches
     end
-    
+
     it 'should render the search results page' do
       do_get
       response.should render_template('search')
