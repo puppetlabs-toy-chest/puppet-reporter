4ede2f1606593aa9610fcb895e468491ccec7291
Kevin R. Barnes <vinbarnes@gmail.com>
2009-02-02 15:54:17 -0600

    Refactor: Cleaning up SearchParser parsing.
    Turning nasty regex into a bunch of smaller segments, building up to the big dog.
    Hopefully this is more readable, easier to follow, and easier to maintain.

--- rails/app/models/search_parser.rb
+++ rails/app/models/search_parser.rb
@@ -1,10 +1,23 @@
 class SearchParser
   class << self
+    WILDCARD = /\*?/
+    VALUE = /[a-z]+/
+    WILD_VALUE = /#{WILDCARD}#{VALUE}#{WILDCARD}/
+    WORDS = /#{VALUE}(?: #{VALUE})*/
+    DQUOTED = /"#{WORDS}"/
+    SQUOTED = /'#{WORDS}'/
+    QUOTED  = /#{DQUOTED}|#{SQUOTED}/
+    KEYWORD = VALUE
+    KEYVALUE = /#{VALUE}|#{QUOTED}/
+    WILD_KEYVALUE = /#{WILDCARD}#{KEYVALUE}#{WILDCARD}/
+    KEYPAIR = /#{KEYWORD}:#{WILD_KEYVALUE}/
+    SEGMENT = /#{KEYPAIR}|#{WILD_VALUE}/
+    
     def parse(query)
       result = Hash.new { |h, k|  h[k] = [] }
       
       if query.match(/:/)
-        query.scan(/[a-z]+:\*?"[a-z ]+"\*?|[a-z]+:\*?'[a-z ]+'\*?|[a-z]+:\*?[a-z]+\*?|\*?[a-z]+\*?/) do |match|
+        query.scan(SEGMENT) do |match|
           key = :name
           if match.match(/:/)
             key, str = match.split(/:/)
