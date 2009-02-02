6fe836fb2840360dd8a8f2cf11b5760c2feb2af9
Kevin R. Barnes <vinbarnes@gmail.com>
2009-02-02 15:02:17 -0600

    Remove recent action from LogsController.
    Removed recent view and spec as well.

--- rails/app/controllers/logs_controller.rb
+++ rails/app/controllers/logs_controller.rb
@@ -1,5 +1,2 @@
 class LogsController < ApplicationController
-  def recent
-    @logs = Log.recent
-  end
 end
--- rails/spec/controllers/logs_controller_spec.rb
+++ rails/spec/controllers/logs_controller_spec.rb
@@ -1,29 +1,4 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe LogsController do
-  describe 'recent' do
-    before :each do
-      @logs = stub('recent logs')
-      Log.stubs(:recent).returns(@logs)
-    end
-    
-    def do_get
-      get :recent
-    end
-    
-    it 'should be successful' do
-      do_get
-      response.should be_success
-    end
-    
-    it 'should get recent logs' do
-      Log.expects(:recent)
-      do_get
-    end
-    
-    it 'should assign recent logs for the view' do
-      do_get
-      assigns[:logs].should == @logs
-    end
-  end
 end
