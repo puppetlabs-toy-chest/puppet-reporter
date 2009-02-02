b1ed0197f064c16d0c3b072615e341353a3a4e51
Kevin R. Barnes <vinbarnes@gmail.com>
2009-02-02 15:31:18 -0600

    Refactor: Cleaning up SearchParser.parse
    Making parsing more obviously similar for name and other keys.
    Handling no-keyword parsing more like keyword parsing.

--- rails/app/models/search_parser.rb
+++ rails/app/models/search_parser.rb
@@ -1,18 +1,22 @@
 class SearchParser
   class << self
     def parse(query)
-      return { :name => query.split(' ').join(',') } unless query =~ /:/
-      result = {}
-      query.scan(/[a-z]+:\*?"[a-z ]+"\*?|[a-z]+:\*?'[a-z ]+'\*?|[a-z]+:\*?[a-z]+\*?|\*?[a-z]+\*?/) do |match|
-        if match.match(/:/)
-          key, str = match.split(/:/)
-          result[key.to_sym] ||= []
-          result[key.to_sym].push str.gsub(/['"]/, '')
-        else
-          result[:name] ||= []
-          result[:name].push match
+      result = Hash.new { |h, k|  h[k] = [] }
+      
+      if query.match(/:/)
+        query.scan(/[a-z]+:\*?"[a-z ]+"\*?|[a-z]+:\*?'[a-z ]+'\*?|[a-z]+:\*?[a-z]+\*?|\*?[a-z]+\*?/) do |match|
+          key = :name
+          if match.match(/:/)
+            key, str = match.split(/:/)
+            key   = key.to_sym
+            match = str.gsub(/['"]/, '')
+          end
+          result[key].push match
         end
+      else
+        result[:name] = query.split(' ')
       end
+      
       result.keys.each { |k|  result[k] = result[k].compact.join(',') }
       result
     end
