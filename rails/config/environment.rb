ce15d780fdcd194d7c0f976e39c3e1adbe1504fe
Rick Bradley <rick@rickbradley.com>
2009-08-03 13:24:17 -0500

    removing vendored gems in favor of an external depedency handling solution

--- rails/config/environment.rb
+++ rails/config/environment.rb
@@ -27,6 +27,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
 
   config.gem "hpricot"
+  config.gem "haml"
   config.gem 'mislav-will_paginate', :version => '~> 2.2.3', :lib => 'will_paginate', :source => 'http://gems.github.com'
   
   # Only load the plugins named here, in the order given. By default, all plugins 
